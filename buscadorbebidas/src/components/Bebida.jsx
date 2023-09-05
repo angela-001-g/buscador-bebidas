f5b845de152fac582966427c5b93c71907222802
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-09-05 16:05:25 -0500

    agregando el componente de Card a las bebidas

--- buscadorbebidas/src/components/Bebida.jsx
+++ buscadorbebidas/src/components/Bebida.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable react/prop-types */
-
+import { Col, Card, Button } from 'react-bootstrap'
 
 function Bebida({bebida}) {
   return (
-    <div>
-      {bebida.strDrink}
-    </div>
+    <Col md={6} lg={3}>
+      <Card className='mb-4'>
+          <Card.Img 
+              variant='top'
+              src={bebida.strDrinkThumb}
+              alt={`Imagen de ${bebida.strDrink}`}
+          />
+          <Card.Body>
+            <Card.Title>{bebida.strDrink}</Card.Title>
+            <Button
+              variant='warning'
+              className='w-100 text-uppercase mt-2'
+            >
+              Ver Receta
+            </Button>
+          </Card.Body>
+      </Card>
+    </Col>
   )
 }
 
